957250667304480eeaa96ef8b4df688467625a59
physrow <61281668+physrow@users.noreply.github.com>
2020-11-07 15:36:52 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ class Rational {
 private:
     int chislitel;
     int  znamenatel;
-    int GCD(int a, int b) const{
+    static int GCD(int a, int b) {
         if (a < 0)
             a*= -1;
         while(b){
@@ -15,7 +15,7 @@ private:
         }
         return a;
     }
-    int LCM(int a, int b)const
+    static int LCM(int a, int b)
     {
         return a*b/ GCD(a,b);
     }
@@ -33,13 +33,12 @@ public:
     }
     Rational() {
         chislitel = 0;
-        znamenatel = 0;
+        znamenatel = 1;
     }
 
     Rational &operator=(const Rational &other) {
         this->chislitel = other.chislitel;
         this->znamenatel = other.znamenatel;
-        sokrawenie();
         return *this;
     }
 
@@ -47,7 +46,7 @@ public:
         return os << rational.chislitel << "/" << rational.znamenatel;
     }
 
-    Rational operator+(const Rational &other) {
+    Rational operator+(const Rational &other) const {
         Rational temp;
         temp.chislitel = this->chislitel + other.chislitel;
         temp.znamenatel = this->znamenatel;
@@ -59,10 +58,10 @@ public:
             temp.chislitel = chislitel + other.chislitel;
             temp.znamenatel = znamenatel;
         }
-        sokrawenie();
+        temp.sokrawenie();
         return temp;
     }
-    Rational operator-(const Rational &other) {
+    Rational operator-(const Rational &other) const {
         Rational temp;
         temp.chislitel = this->chislitel - other.chislitel;
         temp.znamenatel = this->znamenatel;
@@ -74,37 +73,35 @@ public:
             temp.chislitel = chislitel - other.chislitel;
             temp.znamenatel = znamenatel;
         }
-        sokrawenie();
+        temp.sokrawenie();
         return temp;
     }
-    Rational operator/(const Rational &other) {
+    Rational operator/(const Rational &other) const {
         Rational temp;
         temp.chislitel = this->chislitel * other.znamenatel;
         temp.znamenatel = this->znamenatel * other.chislitel;
-        sokrawenie();
+        temp.sokrawenie();
         return temp;
     }
 
-    Rational operator*(const Rational &other) {
+    Rational operator*(const Rational &other) const {
         Rational temp;
         temp.chislitel = this->chislitel * other.chislitel;
         temp.znamenatel = this->znamenatel * other.znamenatel;
-        sokrawenie();
+        temp.sokrawenie();
         return temp;
     }
 
-    Rational &operator*=(const Rational &other) {
+    Rational &operator*=(const Rational &other)  {
         this->chislitel *= other.chislitel;
         this->znamenatel *= other.znamenatel;
         sokrawenie();
         return *this;
     }
-    bool operator==(const Rational &other) {
-        sokrawenie();
+    bool operator==(const Rational &other) const {
         return this->chislitel == other.chislitel and this->znamenatel == other.znamenatel;
     }
-    bool operator!=(const Rational &other) {
-        sokrawenie();
+    bool operator!=(const Rational &other) const {
         return !(this->chislitel == other.chislitel and this->znamenatel == other.znamenatel);
     }
     Rational operator+=(const Rational &other) {
@@ -129,15 +126,38 @@ public:
         return *this;
     }
     bool operator>(Rational second) const {
-        return this->toDouble() > second.toDouble();
+        Rational temp, zero(1,0);
+
+        temp.chislitel = this -> chislitel;
+        temp.znamenatel = this -> znamenatel;
+        if (this -> znamenatel == second.znamenatel){
+            return this -> chislitel > second.chislitel;
+        }
+        if (this -> znamenatel > second.znamenatel){
+            return (temp - second > zero );
+        }
+        if (this -> znamenatel < second.znamenatel){
+            return(temp - second > zero);
+        }
     }
     bool operator<(Rational second) const {
-        return this->toDouble() < second.toDouble();
+        Rational temp, zero(1, 0);
+        temp.chislitel = this->chislitel;
+        temp.znamenatel = this->znamenatel;
+        if (this->znamenatel == second.znamenatel) {
+            return this->chislitel < second.chislitel;
+        }
+        if (this->znamenatel > second.znamenatel) {
+            return (temp - second < zero);
+        }
+        if (this->znamenatel < second.znamenatel) {
+            return (temp - second < zero);
+        }
     }
     double toDouble() const {
         return (double) this->chislitel / this->znamenatel;
     }
-    Rational operator-=(const Rational &other) {
+    Rational operator-=(const Rational &other)  {
         Rational temp;
         temp.chislitel = this->chislitel -= other.chislitel;
         temp.znamenatel = this->znamenatel;
@@ -156,14 +176,14 @@ public:
     Rational operator-() const {
         return Rational(-this->chislitel, znamenatel);
     }
-    Rational POWx2(){
+    Rational POWx2() const{
         Rational temp;
         temp.chislitel = this -> chislitel * this ->chislitel;
         temp.znamenatel = this -> znamenatel * this -> znamenatel;
-        sokrawenie();
+        temp.sokrawenie();
         return temp;
     }
-    Rational POW(Rational value, int num){
+    static Rational POW(Rational value, int num){
         Rational temp;
         temp.chislitel = value.chislitel;
         temp.znamenatel = value.znamenatel;
@@ -171,18 +191,18 @@ public:
             temp.chislitel *= value.chislitel;
             temp.znamenatel *= value.znamenatel;
         }
-        sokrawenie();
+        temp.sokrawenie();
         return temp;
     }
-    Rational POW(Rational value, double num){
+    Rational POW(Rational value, double num) const{
         Rational temp;
         temp.chislitel *= 0.5;
         temp.znamenatel *= 0.5;
-        sokrawenie();
+        temp.sokrawenie();
         return temp;
     }
 
-    Rational sqrt(){
+    Rational sqrt() const{
         return Rational(::sqrt(chislitel),::sqrt(znamenatel));
     }
 };
@@ -219,28 +239,28 @@ public:
     friend ostream &operator<<(ostream &os, const ComplexNumber complexNumber) {
         return os << "z = " << complexNumber.Real << " + (" << complexNumber.Imag << ")*i";
     }
-    ComplexNumber operator+(const ComplexNumber &other) {
+    ComplexNumber operator+(const ComplexNumber &other) const {
         return ComplexNumber(this->Real + other.Real, this->Imag + other.Imag);
     }
-    ComplexNumber operator-(const ComplexNumber &other) {
+    ComplexNumber operator-(const ComplexNumber &other) const{
         return ComplexNumber(this->Real - other.Real, this->Imag - other.Imag);
     }
-    ComplexNumber operator*(const ComplexNumber &other) {
+    ComplexNumber operator*(const ComplexNumber &other) const {
         ComplexNumber temp;
         temp.Real = (((this->Real) * (other.Real)) - ((this->Imag) * (other.Imag)));
         temp.Imag = (((this->Real) * (other.Imag)) + ((this->Imag) * (other.Real)));
         return temp;
     }
-    ComplexNumber operator/(ComplexNumber& other ) {
+    ComplexNumber operator/(ComplexNumber& other ) const {
         ComplexNumber temp;
         temp.Real = (this->Real*other.Real+this->Imag*other.Imag) / (other.Real*other.Real+other.Imag*other.Imag);
         temp.Imag = (this->Imag*other.Real-this->Real*other.Imag) / (other.Real*other.Real+other.Imag*other.Imag);
         return temp;
     }
-    bool operator==(const ComplexNumber& other){
+    bool operator==(const ComplexNumber& other) const{
         return this -> Real == other.Real and this -> Imag == other.Imag;
     }
-    bool operator!=(const ComplexNumber& other){
+    bool operator!=(const ComplexNumber& other) const{
         return !(this->Real == other.Real and this->Imag == other.Imag);
     }
     ComplexNumber &operator+=(const ComplexNumber &other) {
@@ -263,10 +283,10 @@ public:
         this->Imag /= other.Imag;
         return *this;
     }
-    ComplexNumber operator-() {
+    ComplexNumber operator-() const {
         return ComplexNumber(-(this->Real), (-this->Imag));
     }
-    double abs() {
+    double abs() const {
         ComplexNumber temp;
         Rational a(1,2);
         temp.Real = this->Real * this->Real;
@@ -275,7 +295,7 @@ public:
         tempe = temp.Real*temp.Real + temp.Imag * temp.Imag;
         return ::sqrt(tempe.toDouble());
     }
-    double arg() {
+    double arg() const{
         double pi = 3.14;
         if (this->Real.toDouble() == 0 and this->Imag.toDouble() == 0)
             return 0;
@@ -289,7 +309,7 @@ public:
             return pi / 2;
         else return -pi / 2;
     }
-    ComplexNumber POW(ComplexNumber val,int num){
+    static ComplexNumber POW(ComplexNumber val,int num) {
         ComplexNumber finally;
         finally = val;
         for (int i = 0; i <= num ; i++){
@@ -299,17 +319,12 @@ public:
     }
 
 };
-// READY FOR RATIONAL : ALL OVERLOAD/ ALL FOO
-// READY FOR COMPLEX : ALL OVERLOAD / ALL FOO / KROME POW ;(
+// READY FOR RATIONAL : ALL OVERLOAD/ ALL FOO/UPD FIX
+// READY FOR COMPLEX : ALL OVERLOAD / ALL FOO / UPP POW/APD ALL FIX
 int main() {
-    Rational a(1, 2);
-    Rational b(1, 3);
+    Rational a(4, 2);
+    Rational b(14, 2);
     Rational c;
     c = a + b;
-    c = a - b;
-    c = a * b;
-    c = a / b;
-    ComplexNumber a1(a,b);
-    a1 += a1;
-    cout << a1;
+    cout << c;
 }
